3875bb9adfd457d13e9a0afc27e45ac661ba0ac6
bbulgu <bbulgudacity@gmail.com>
2020-08-19 11:11:48 +0300

    All requirements met

--- flaskiburada.py
+++ flaskiburada.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, flash, url_for, redirect
+from flask import Flask, render_template, flash, url_for, redirect, request
 from forms import ProductForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -17,6 +17,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config['APP']['SQLALCHEMY_DATABASE_URI']
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+def getProduct(productID):
+    return ProductsModel.query.get(productID)
+
+def allProducts():
+    return ProductsModel.query.all()
+
+def deleteProduct(productID):
+    ProductsModel.query.filter_by(id=productID).delete()
+    db.session.commit()
+
+def addProductToDbFromLink(productLink):
+    r = requests.get(productLink)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    productName = soup.find('h1', attrs={'class' : 'product-name'}).text.strip()
+    product_price = soup.find('span', attrs={'class': 'price'}).get("content")
+    product_image = soup.find('img', attrs={'class': 'product-image'}).get("src")
+    
+    product = ProductsModel(price=product_price, image=product_image, title=productName)
+    db.session.add(product)
+    db.session.commit()
+    return product
+
 class ProductsModel(db.Model):
     __tablename__ = 'products'
 
@@ -33,8 +55,6 @@ class ProductsModel(db.Model):
     def __repr__(self):
         return f"<Product {self.title}>"
 
-db.create_all()
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
@@ -42,39 +62,30 @@ def home():
     if form.validate_on_submit():
         flash(f'The link {form.product_link.data} is an actual product on hepsiburada!', 'success')
         # process link here
-        fileLink = form.product_link.data.split('.com/')[1]
-        return redirect(url_for('details', product=fileLink)) 
+        productID = addProductToDbFromLink(form.product_link.data).id
+        return redirect(url_for('details', productID=productID)) 
     return render_template('home.html', form=form)
 
-@app.route('/details/link?<product>')
-def details(product):
-    r = requests.get('http://www.hepsiburada.com/' + product)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    productName = soup.find('h1', attrs={'class' : 'product-name'}).text.strip()
-    product_price = soup.find('span', attrs={'class': 'price'}).get("content")
-    product_image = soup.find('img', attrs={'class': 'product-image'}).get("src")
-    
-    product = ProductsModel(price=product_price, image=product_image, title=productName)
-    db.session.add(product)
-    db.session.commit()
+@app.route('/details/link?<productID>')
+def details(productID):
+    return render_template('details.html', product=getProduct(productID))
 
-    print(product_price)
-    print(f"Image {product_image}")
-    return render_template('details.html', product=product)
-
-@app.route('/listproducts')
+@app.route('/listproducts', methods=["GET", "POST"])
 def listProducts():
-    products = ProductsModel.query.all()
-    results = [
-            {
-                "id": product.id,
-                "price": product.price,
-                "image": product.image,
-                "title": product.title
-            } for product in products]
-    return render_template('products.html', results=results)
-    
+    page = request.args.get('page', 1, type=int)
+    products = ProductsModel.query.paginate(page=page, per_page=3)
+    print("list")
 
+    if request.method == "POST":
+        print(request.form)   
+        print("sup")
+    return render_template('products.html', results=products)
+
+@app.route('/delete/<productID>', methods=["GET", "POST", "DELETE"])
+def delete(productID):
+    productID = int(productID)
+    deleteProduct(productID)
+    return redirect(url_for('listProducts', page=1)) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
